206f67d0db16292f85bd949973adf555e938999e
LucasNotz <lucas.fontana@sempreceub.com>
2025-05-10 09:40:38 -0300

    Script to be forked

---
+++ script.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+if __name__ == '__main__':
+
+    window = tk.Tk()
+    window.title("Projeto colaboração")
+    window.geometry("700x500")
+
+    label = tk.Label(master = window, text = "Projeto colaboração")
+    label.pack(pady = 10)
+
+    text = tk.Text(master = window)
+    text.pack()
+
+
+    window.mainloop()
+
